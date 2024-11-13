c7e50ffd643d2560d55a4fd2f5ee13225c47cb80
Dario Cabezas <dariosce99@gmail.com>
2024-11-13 10:06:22 -0500

    DC: Endpoints dashbord listos

--- app/models/dashboards.py
+++ app/models/dashboards.py
@@ -9,7 +9,6 @@ class DashboardBase(SQLModel):
 
 class Dashboard(DashboardBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    user_id: int = Field(foreign_key="user.id")
 
     boards: List["Board"] = Relationship(
         back_populates="dashboards", link_model=DBoards
