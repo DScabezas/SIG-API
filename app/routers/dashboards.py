2340dfb8117cfbfde41cb72791eeb28d19b79b18
Dario Cabezas <dariosce99@gmail.com>
2024-11-14 11:15:17 -0500

    DC: Mejora de endpoints y endpoint delete

--- app/routers/dashboards.py
+++ app/routers/dashboards.py
@@ -19,21 +19,17 @@ def create_dashboard(user_id: int, session: SessionDep) -> Dashboard:
     Crea un nuevo dashboard para un usuario.
 
     - **user_id**: ID del usuario para el cual se creará el dashboard.
-    - **session**: Dependencia de la sesión de base de datos.
 
     Verifica si el usuario existe y si ya tiene un dashboard. Si el usuario no existe o ya tiene un dashboard, lanza una excepción.
     Si el usuario es válido y no tiene un dashboard, se crea uno nuevo.
 
     Retorna el dashboard recién creado con un código de estado 201.
     """
-    user_exists = session.exec(select(User).where(User.id == user_id)).first()
-    if not user_exists:
+    user = session.exec(select(User).where(User.id == user_id)).first()
+    if not user:
         raise HTTPException(status.HTTP_404_NOT_FOUND, detail="User not found")
 
-    existing_dashboard = session.exec(
-        select(Dashboard).where(Dashboard.user_id == user_id)
-    ).first()
-    if existing_dashboard:
+    if session.exec(select(Dashboard).where(Dashboard.user_id == user_id)).first():
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="User already has a dashboard",
@@ -53,31 +49,52 @@ def create_dashboard(user_id: int, session: SessionDep) -> Dashboard:
     status_code=status.HTTP_200_OK,
     tags=["Dashboards"],
 )
-def get_user_dashboard(user_id: int, session: SessionDep):
+def get_user_dashboard(user_id: int, session: SessionDep) -> Dashboard:
     """
     Obtiene el Dashboard de un Usuario.
     """
     user = session.exec(select(User).where(User.id == user_id)).first()
-    if not user:
+    if not user or not user.dashboard:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+            status_code=status.HTTP_404_NOT_FOUND, detail="User or Dashboard not found"
         )
+    return user.dashboard
 
-    dashboard = user.dashboard
+
+@router.delete(
+    "/dashboards/{dashboard_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    tags=["Dashboards"],
+)
+def delete_dashboard(dashboard_id: int, session: SessionDep) -> None:
+    """
+    Elimina un Dashboard si no tiene ningún Board asociado.
+
+    - **dashboard_id**: ID del dashboard a eliminar.
+
+    Verifica si el dashboard existe y si tiene algún board asociado. Si tiene boards, lanza una excepción.
+    Si no tiene boards, elimina el dashboard y retorna un código de estado 204.
+    """
+    dashboard = session.exec(
+        select(Dashboard).where(Dashboard.id == dashboard_id)
+    ).first()
     if not dashboard:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Dashboard not found")
+
+    if dashboard.boards:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Dashboard not found"
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Dashboard has associated boards and cannot be deleted",
         )
 
-    return dashboard
+    session.delete(dashboard)
+    session.commit()
 
 
 @router.get("/dashboards/", response_model=list[DashboardRead], tags=["Dashboards"])
 def list_dashboards(session: SessionDep) -> list[DashboardRead]:
     """
     Lista todos los dashboards con sus boards asociados.
-
-    - **session**: Dependencia de la sesión de base de datos.
 
     Retorna una lista con todos los dashboards y sus boards en la base de datos.
     """
